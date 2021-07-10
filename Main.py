2ffda4b5b7de8ee0f6c16c059477fbe173565d54
rajabi2001 <mj.rajabi2001@gmail.com>
2021-07-10 11:53:01 +0430

    Filter PosComments

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 import io
 import re
+from collections import defaultdict
 
 poscomment = []
 negcomment = []
-
+dicpos = defaultdict(int)
+dicneg = defaultdict(int)
 
 address = ".\Sources\\rt-polarity.pos"
 with open(address) as reader :
@@ -15,12 +17,17 @@ buf = io.StringIO(myinput)
 line = buf.readline()
 
 while line != "":
-    line = re.sub(",|\?|\.|\[|\]|\"|-|;|:|\(|\)|\\|_|\*|&|\^|\$|!" , " " , line)
+    line = re.sub(",|\?|\.|\[|\]|\"|-|;|:|\(|\)|\\|_|\*|&|\^|\$|!|'|\/|–" , " " , line)
     line = re.sub("\s{2,}"," ",line)
+    line = re.sub("\n","",line)
+    buffread = re.findall("[a-zA-Z0-9]+", line)
+    poscomment.append(buffread)
     line = buf.readline()
 
     
+for i in poscomment:
+    for ii in i:
+        if len(ii) >= 2:
+            dicpos[ii] += 1
 
-# for i in poscomment:
-#     print(poscomment)
 
