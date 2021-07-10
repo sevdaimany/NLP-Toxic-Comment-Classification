8645a232cf0de31b9286bd122c07d1873f97aae5
rajabi2001 <mj.rajabi2001@gmail.com>
2021-07-10 10:25:10 +0430

    Filing

---
+++ Main.py
@@ -0,0 +1,16 @@
+import io
+
+
+address = ".\puzzles\puzzle3.txt"
+with open(address) as reader :
+    myinput = reader.read()
+
+
+buf = io.StringIO(myinput)
+n , m = map(int ,buf.readlines(1)[0].replace("\t" , " ").replace("\n" , "").split(" "))
+for i in range(n):
+    buffread = buf.readlines(1)[0].replace("\t" , " ").replace("\n" , "").split(" ")
+    mynd = []
+    for ii in range(m) : 
+        mynd.append(buffread[ii])
+    mypuzzle.append(mynd)
