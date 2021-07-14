c49fc12b0bf19ac95ca09a03a777521070f264e0
rajabi2001 <mj.rajabi2001@gmail.com>
2021-07-14 14:02:18 +0430

    Add list to dict AND Add binary

--- Main.py
+++ Main.py
@@ -4,8 +4,11 @@ from collections import defaultdict
 
 poscomment = []
 negcomment = []
-dicpos = defaultdict(int)
-dicneg = defaultdict(int)
+dicpos = defaultdict(list)
+dicneg = defaultdict(list)
+dicposbin = defaultdict(list)
+dicnegbin= defaultdict(list)
+
 
 address = ".\Sources\\rt-polarity.pos"
 with open(address) as reader :
@@ -23,16 +26,43 @@ while line != "":
     poscomment.append(buffread)
     line = buf.readline()
 
-
 for i in poscomment:
     for ii in i:
+        if ii not in dicpos.keys():
+            dicpos[ii].append(0)
+
         if len(ii) >= 2:
-            dicpos[ii] += 1
+            dicpos[ii][0] += 1
+
+for i in poscomment:
+    for ii in range(len(i)):
+        if ii == (len(i) - 1):
+            break
+        if len(i[ii]) < 3 or  len(i[ii+1]) < 3:
+            continue
+        binary = i[ii] +" "+ i[ii+1]
+        if binary not in dicposbin.keys():
+            dicposbin[binary].append(0)
+
+        if len(binary) > 5:
+            dicposbin[binary][0] += 1
+
+
 
 for i in poscomment:
     for ii in i:
-        if dicpos[ii] >= 10 or dicpos[ii]  <= 2:
-            dicpos.pop(ii)
+        if ii in dicpos.keys():
+            if dicpos[ii][0] > 10 or dicpos[ii][0]  < 3:
+                dicpos.pop(ii)
+
+for i in poscomment:
+    for ii in range(len(i)):
+        if ii == (len(i) - 1):
+            break
+        binary = i[ii] +" "+ i[ii+1]
+        if binary in dicposbin.keys():
+            if dicposbin[binary][0] > 10 or dicposbin[binary][0]  < 3:
+                dicposbin.pop(binary)
 
 
 
@@ -54,13 +84,40 @@ while line != "":
 
 for i in negcomment:
     for ii in i:
+        if ii not in dicneg.keys():
+            dicneg[ii].append(0)
+
         if len(ii) >= 2:
-            dicneg[ii] += 1
+            dicneg[ii][0] += 1
+
+
+for i in negcomment:
+    for ii in range(len(i)):
+        if ii == (len(i) - 1):
+            break
+        if len(i[ii]) < 3 or  len(i[ii+1]) < 3:
+            continue
+        binary = i[ii] +" "+ i[ii+1]
+        if binary not in dicnegbin.keys():
+            dicnegbin[binary].append(0)
+
+        if len(binary) > 5:
+            dicnegbin[binary][0] += 1
+
 
 for i in negcomment:
     for ii in i:
-        if dicneg[ii] >= 10 or dicneg[ii]  <= 2:
-            dicneg.pop(ii)
+        if ii in dicneg.keys():
+            if dicneg[ii][0] >= 10 or dicneg[ii][0]  <= 2:
+                dicneg.pop(ii)
 
 
+for i in negcomment:
+    for ii in range(len(i)):
+        if ii == (len(i) - 1):
+            break
+        binary = i[ii] +" "+ i[ii+1]
+        if binary in dicnegbin.keys():
+            if dicnegbin[binary][0] > 10 or dicnegbin[binary][0]  < 3:
+                dicnegbin.pop(binary)
 
